f03862cc143353d325b4ef5cc1911e538d958322
tarsobcaldas <tarsobcaldas@gmail.com>
2023-05-21 20:57:47 +0100

    recursively add files for servers and plugins

--- lua/noa/init.lua
+++ lua/noa/init.lua
@@ -2,3 +2,21 @@ require("noa.settings")
 require("noa.plugins")
 require("noa.mappings")
 require("noa.lsp")
+
+
+local function source_files_from_dir(directory)
+  for _, file in pairs(vim.fn.readdir(directory)) do
+    local file = directory .. '/' .. file
+    if vim.fn.filereadable(file) then
+      vim.fn.execute('source ' .. file)
+    end
+  end
+end
+
+-- load all additional configs
+local lua_config_dir = vim.fn.stdpath('config') .. '/lua/noa/'
+local config_dirs = { 'plugins', 'lsp/settings' }
+
+for _, dir in pairs(config_dirs) do
+  source_files_from_dir(lua_config_dir .. '/' .. dir)
+end
--- lua/noa/lsp/servers.lua
+++ lua/noa/lsp/servers.lua
@@ -12,12 +12,3 @@ require("mason-lspconfig").setup {
   automatic_installation = {exclude = {"hls"}}
 }
 
-require("noa.lsp.settings.ltex")
-require("noa.lsp.settings.texlab")
-require("noa.lsp.settings.remark_ls")
-require("noa.lsp.settings.lua_ls")
-require("noa.lsp.settings.perlnavigator")
-require("noa.lsp.settings.html")
-require("noa.lsp.settings.yamlls")
-require("noa.lsp.settings.clanged")
-require("noa.lsp.settings.hls")
