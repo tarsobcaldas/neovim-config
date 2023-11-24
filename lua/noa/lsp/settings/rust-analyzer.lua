346489b0343e205a29ce94ce1aea715e8d87c0f5
tarsobcaldas <tarsobcaldas@gmail.com>
2023-11-24 21:24:44 +0000

    Configures support for Rust

---
+++ lua/noa/lsp/settings/rust-analyzer.lua
@@ -0,0 +1,7 @@
+local on_attach = require("noa.lsp.handlers").on_attach
+local capabilities = require("noa.lsp.handlers").capabilities
+
+require'lspconfig'.rust_analyzer.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
